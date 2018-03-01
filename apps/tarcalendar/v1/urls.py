b09686e740380de9e9bbae2f1bf583fd2f38776a
Marc Pittinsky <marclane@live.unc.edu>
2018-02-28 23:23:35 -0500

    connected to google sheets

--- apps/tarcalendar/v1/urls.py
+++ apps/tarcalendar/v1/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^request/', views.UserCalendarRequestView.as_view(), name='cal-request'),
+    url(r'^request', views.UserCalendarRequestView.as_view(), name='cal-request'),
 ]
--- apps/tarcalendar/v1/views.py
+++ apps/tarcalendar/v1/views.py
@@ -1,6 +1,9 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from apps.users.models import UserProfile
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 import pygsheets
 
@@ -10,8 +13,19 @@ class UserCalendarRequestView(APIView):
 	authentication_classes = (IsAuthenticated,)
 
 	def get(self,request):
-		gc = pygsheets.authorize()
-		sh = gc.open("Targenda Calendar Requests")
-		wks = sh.worksheet_by_title("Sheet1")
-		for row in wks:
-  			cd print(row)
+		try:
+			gc = pygsheets.authorize()
+			sh = gc.open("Targenda Calendar Requests")
+			wks = sh.worksheet_by_title("Sheet1")
+			values = []
+			pk = self.request.query_params.get('id', None)
+			profile = get_object_or_404(UserProfile,user= pk)
+			user = get_object_or_404(User,pk=pk)
+			values.append(user.email)
+			for course in profile.courses.all():
+				values.append('{} {}-{} {} ({})'.format(course.department,course.number,course.section,course.description,course.semester))
+			values.append("new")
+			wks.insert_rows(row=0, number=1, values=values)
+			return Response({"success":True})
+		except Exception as err:
+			raise err
