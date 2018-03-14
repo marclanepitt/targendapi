a2237e4e16a67b2cfa4ff9a14c50222243e7c9c8
Marc Pittinsky <marclane@live.unc.edu>
2018-03-14 01:13:04 -0400

    emails

--- targendaapi/settings/production_settings.py
+++ targendaapi/settings/production_settings.py
@@ -1,7 +1,7 @@
 import os
 from .settings import BASE_DIR
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST','*')]
 
 EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER')
