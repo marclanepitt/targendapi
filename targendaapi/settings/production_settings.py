1410d8c1b32eb7fef8c6c50d91f4bd5418831f18
Marc Pittinsky <marclane@live.unc.edu>
2018-03-14 10:48:59 -0400

    production settings

--- targendaapi/settings/production_settings.py
+++ targendaapi/settings/production_settings.py
@@ -1,4 +1,5 @@
 import os
+from .settings import INSTALLED_APPS
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
@@ -6,3 +7,6 @@ EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER')
 EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN')
 EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD')
 EMAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT')
+INSTALLED_APPS = INSTALLED_APPS + (
+    'raven.contrib.django.raven_compat',
+)
