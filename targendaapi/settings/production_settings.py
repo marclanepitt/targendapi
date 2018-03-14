013717862e288877bc9b13c3bf41efbeafcdc861
Marc Pittinsky <marclane@live.unc.edu>
2018-03-14 01:19:29 -0400

    emails

--- targendaapi/settings/production_settings.py
+++ targendaapi/settings/production_settings.py
@@ -2,7 +2,7 @@ import os
 from .settings import BASE_DIR
 
 CORS_ORIGIN_ALLOW_ALL = True
-ALLOWED_HOSTS = ['class-cal-api.herokuapp.com']
+ALLOWED_HOSTS = ['class-cal-api.herokuapp']
 
 EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER')
 EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN')
