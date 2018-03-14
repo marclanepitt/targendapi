98ad7adea243412a67cee135da11a9bca8e4f8ad
Marc Pittinsky <marclane@live.unc.edu>
2018-03-14 11:45:55 -0400

    production settings

--- targendaapi/settings/settings.py
+++ targendaapi/settings/settings.py
@@ -279,10 +279,6 @@ LOGGING = {
     }
 }
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
 
 if(os.environ.get('PRODUCTION')):
