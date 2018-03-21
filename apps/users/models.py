60c47f093673cc2b7972962615912591eb7800c5
Marc Pittinsky <marclane@live.unc.edu>
2018-03-21 00:30:27 -0400

    fixed cascade on cal request delete

--- apps/users/models.py
+++ apps/users/models.py
@@ -7,7 +7,7 @@ from ..tarcalendar.models import CalendarRequest
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     courses = models.ManyToManyField(Course)
-    cal_request = models.OneToOneField(CalendarRequest, on_delete=models.CASCADE,null=True)
+    cal_request = models.OneToOneField(CalendarRequest, on_delete=models.SET_NULL,null=True)
 
     def __str__(self):
         return '{} {}'.format(self.user.first_name, self.user.last_name)
